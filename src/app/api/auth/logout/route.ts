da61b907e0897920edd1abf6a917792ba888996c
mawarrt002ciptaland <mawarrt002.ciptaland@gmail.com>
2026-09-22 17:53:28 +0700

    Delete src directory

--- src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  const res = NextResponse.json({ success: true, message: "Logged out" });
-  res.cookies.delete("kas_rt_user");
-  return res;
-}
